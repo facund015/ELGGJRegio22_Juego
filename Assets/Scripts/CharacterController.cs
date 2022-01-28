0b77f17b94b7d5d39c631da57573053f2c50265b
facund015 <A01283666@itesm.mx>
2022-01-27 19:34:56 -0600

    Character Movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterController : MonoBehaviour
 {
     public float floatingSpeed;
     public float walkingSpeed;
-    
+    public float speed;
+
     private Rigidbody2D rb;
     private Vector2 movement;
     private bool gravitySwitch = false;
+    public bool hasArmor = false;
+    private bool touchingEnemy = false;
+    private int iFrames = 30;
 
     private void Start()
     {
@@ -31,29 +36,75 @@ public class CharacterController : MonoBehaviour
         }
 
         //Activa la gravedad
-        if (Input.GetKeyDown(KeyCode.G) && !gravitySwitch)
-        {
-            rb.gravityScale = 50;
-            gravitySwitch = true;
-        }
-        //Desactiva la gravedad
-        else if (Input.GetKeyDown(KeyCode.G) && gravitySwitch)
-        {
-            rb.gravityScale = 0;
-            gravitySwitch = false;
-        }
+        //if (hasArmor)
+        //{
+        //    rb.gravityScale = 50;
+        //    gravitySwitch = true;
+        //}
+        ////Desactiva la gravedad
+        //else if (!hasArmor)
+        //{
+        //    rb.gravityScale = 0;
+        //    gravitySwitch = false;
+        //}
     }
 
     //Actualiza el motor de fisicas en un timer constante
     private void FixedUpdate()
     {
-        if (!gravitySwitch)
+        //if (!gravitySwitch)
+        //{
+        //    rb.MovePosition(rb.position + movement.normalized * floatingSpeed * Time.fixedDeltaTime);
+        //}
+        //else
+        //{
+        //    rb.MovePosition(rb.position + movement.normalized * walkingSpeed * Time.fixedDeltaTime);
+        //}
+
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+        Debug.Log(movement);
+
+        if (touchingEnemy)
         {
-            rb.MovePosition(rb.position + movement.normalized * floatingSpeed * Time.fixedDeltaTime);
+            iFrames--;
+            if (iFrames == 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
-        else
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Armor")
         {
-            rb.MovePosition(rb.position + movement.normalized * walkingSpeed * Time.fixedDeltaTime);
+            Destroy(collision.gameObject);
+            //rb.AddForce(Vector3.up * 100000000);
+            gravitySwitch = true;
+            rb.gravityScale = 50;
+            hasArmor = true;
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            touchingEnemy = true;
+            if (hasArmor)
+            {
+                hasArmor = false;
+                gravitySwitch = false;
+                rb.gravityScale = 0;
+            } else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            touchingEnemy = false;
+            iFrames = 30;
         }
     }
 }
