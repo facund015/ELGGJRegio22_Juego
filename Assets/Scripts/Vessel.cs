b3eb9c430f3aa99240cc71532925616f6885ef42
facund015 <A01283666@itesm.mx>
2022-01-29 16:43:01 -0600

    Revert "revert"
    This reverts commit 5ee3f03a41855732116e64144f118f6613d90ba6.

--- Assets/Scripts/Vessel.cs
+++ Assets/Scripts/Vessel.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Armor : MonoBehaviour
+public class Vessel : MonoBehaviour
 {
 
     private Vector2 originPosition;
+    public bool hasMirror;
+    public GameObject mirror;
     // Start is called before the first frame update
     void Start()
     {
